b383083434a6d0b841898fe809030722445bb1d3
YasminSLobo <125031166+YasminLobo@users.noreply.github.com>
2024-11-07 18:48:20 -0300

    .

--- Forum-Igor/index.php
+++ Forum-Igor/index.php
@@ -5,7 +5,7 @@ session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "senior_care";
+$dbname = "seniorcare";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
